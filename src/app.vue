47159e44a96296525caa643244407b98e0ddb95d
皛白 <xiaobai@fordeal.com>
2020-06-06 17:06:25 +0800

    feat: webpack+vue dev env config

---
+++ src/app.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Demo</h1>
+        <hello-ssr />
+        <img :src="FdLogo" alt="" />
+        <img :src="RedLogo" alt="" />
+    </div>
+</template>
+
+<script>
+import HelloSsr from '@/components/hello-ssr.vue';
+import FdLogo from '@/assets/images/f-200.png';
+import RedLogo from '@/assets/images/logo-round.png';
+export default {
+    name: 'app',
+    data() {
+        return {
+            FdLogo,
+            RedLogo,
+        };
+    },
+    components: {
+        HelloSsr,
+    },
+};
+</script>
---
+++ src/components/hello-ssr.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        SSR components
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ssr',
+};
+</script>
