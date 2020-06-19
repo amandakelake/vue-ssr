7aac8fa697fd5ea2d7d50805a73b786b6a332fc0
皛白 <xiaobai@fordeal.com>
2020-06-19 11:22:50 +0800

    Revert "test: test server"
    This reverts commit 9b97470168d03be0fd093939069e316471b776cd.

--- server/server.js
+++ server/server.js
@@ -9,18 +9,58 @@ const backendRouter = new Router();
 const frontendRouter = new Router();
 const { createBundleRenderer } = require('vue-server-renderer');
 
-backendRouter.get('/', async (ctx, next) => {
-    try {
-        ctx.type = 'html';
-        ctx.status = 200;
-        ctx.body = 'SSR';
-    } catch (error) {
-        console.error(error);
-        ctx.status = 500;
-        ctx.body = '服务器内部错误';
-    }
+const serverBundle = require(path.resolve(__dirname, '../dist/vue-ssr-server-bundle.json'));
+const clientManifest = require(path.resolve(__dirname, '../dist/vue-ssr-client-manifest.json'));
+const template = fs.readFileSync(path.resolve(__dirname, '../dist/index.ssr.html'), 'utf-8');
+const renderer = createBundleRenderer(serverBundle, {
+    runInNewContext: false,
+    template: template,
+    clientManifest: clientManifest,
 });
 
-backendApp.listen(3300, () => {
-    console.log('服务器端渲染地址： http://localhost:3300');
+const handleBackendRoute = (ctx, next) => {
+    console.log('ctx', ctx);
+    console.log('url', ctx.url);
+
+    let context = {
+        url: ctx.url,
+    };
+
+    const ssrStream = renderer.renderToStream(context);
+    ctx.status = 200;
+    ctx.type = 'html';
+    ctx.body = ssrStream;
+};
+
+// koa-router 9.0 不能使用通配符 * , 主要为了 `path-to-regexp` 为了兼容 Express <= 4.x
+// https://github.com/koajs/router/issues/76
+// https://github.com/pillarjs/path-to-regexp#compatibility-with-express--4x
+backendRouter.get('/', handleBackendRoute);
+backendRouter.get('/bar', handleBackendRoute);
+backendRouter.get('/foo', handleBackendRoute);
+
+backendApp.use(serve(path.resolve(__dirname, '../dist')));
+backendApp.use(backendRouter.routes()).use(backendRouter.allowedMethods());
+
+backendApp.listen(3000, () => {
+    console.log('服务器端渲染地址： http://localhost:3000');
+});
+
+const handleFrontendRoute = (ctx, next) => {
+    let html = fs.readFileSync(path.resolve(__dirname, '../dist/index.html'), 'utf-8');
+    ctx.type = 'html';
+    ctx.status = 200;
+    ctx.body = html;
+};
+// 前端Server
+frontendRouter.get('/', handleFrontendRoute);
+frontendRouter.get('/bar', handleFrontendRoute);
+frontendRouter.get('/foo', handleFrontendRoute);
+
+frontendApp.use(serve(path.resolve(__dirname, '../dist')));
+
+frontendApp.use(frontendRouter.routes()).use(frontendRouter.allowedMethods());
+
+frontendApp.listen(3001, () => {
+    console.log('浏览器端渲染地址： http://localhost:3001');
 });
